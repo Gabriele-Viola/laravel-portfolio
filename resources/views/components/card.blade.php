3027b5dede679435829d29f8ff8b83dd67e316df
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-03-06 17:14:02 +0100

    add delete function

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -35,9 +35,34 @@
                 <a class="btn btn-outline-secondary shadow" href={{ route("projects.edit", $project) }}><i class="bi bi-pencil"></i></a>
             </div>
             <div>
-                <a class="btn btn-danger shadow" href={{ route("projects.show", $project) }}><i class="bi bi-trash"></i></a>
+                <button type="button" class="btn btn-danger shadow" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                    <i class="bi bi-trash"></i>
+                  </button>
+                
             </div>
         </div>
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="deleteModalLabel">Are you sure to delete this project?</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    {{$project->title}}
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><span class="text-capitalize">no please...</span></button>
+                  <form action="{{route("projects.destroy", $project)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger shadow" ><span class="text-uppercase">delete for ever!</span></button>
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
     {{$slot}}
 </div>
